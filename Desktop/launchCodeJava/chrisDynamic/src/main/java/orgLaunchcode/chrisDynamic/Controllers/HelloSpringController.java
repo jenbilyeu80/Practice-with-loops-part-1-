52755814f9ff4e989cc63eb4289bb1d46df2420c
jenbilyeu80 <125934039+jenbilyeu80@users.noreply.github.com>
2023-07-15 23:03:57 -0500

    FirstCommit

---
+++ Desktop/launchCodeJava/chrisDynamic/src/main/java/orgLaunchcode/chrisDynamic/Controllers/HelloSpringController.java
@@ -0,0 +1,41 @@
+package orgLaunchcode.chrisDynamic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloSpringController {
+    @RequestMapping(value = "hello", method ={RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model){
+        String greeting ="Hello, " +name +"!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model){
+        String greeting ="Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "Hello";
+    }
+    @GetMapping("form")
+    public String helloForm(){
+
+        return "form";
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        names.add("Ruby");
+        model.addAttribute("names", names);
+        return "hello-list";
+
+    }
+}
---
+++ Desktop/launchCodeJava/chrisDynamic/src/test/java/orgLaunchcode/chrisDynamic/ChrisDynamicApplicationTests.java
@@ -0,0 +1,13 @@
+package orgLaunchcode.chrisDynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrisDynamicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
