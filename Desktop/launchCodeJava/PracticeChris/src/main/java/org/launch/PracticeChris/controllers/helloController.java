e9034e2ab173f12f5207a11bb7dab9bc8b2e4cc8
jenbilyeu80 <125934039+jenbilyeu80@users.noreply.github.com>
2023-07-15 19:16:35 -0500

    Goodbye

--- Desktop/launchCodeJava/PracticeChris/src/main/java/org/launch/PracticeChris/controllers/helloController.java
+++ Desktop/launchCodeJava/PracticeChris/src/main/java/org/launch/PracticeChris/controllers/helloController.java
@@ -2,20 +2,40 @@ package org.launch.PracticeChris.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.websocket.server.PathParam;
+
 @Controller
+@ResponseBody
 public class helloController {
 
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return"Hello, Spring!";
-    }
+    //@GetMapping("hello")
+    //@ResponseBody
+    //public String hello(){
+      //  return"Hello, Spring!";
+    //}
 
     @GetMapping("goodbye")
-    @ResponseBody
-    public String goodbye(){
-        return"Goodbye, Spring!";
+    public String goodbye(){return"Goodbye, Spring!";}
+    @GetMapping("hello")
+    public String helloWithQueryParam(@RequestParam String name){return "Hello, " + name + "!";}
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello,  " + name + "!";
+    }
+    @GetMapping("form")
+    public String helloForm(){
+       return  "<html>" +
+               "<body>" +
+               "<form action= 'hello'>" +
+               "<input type= 'text' name = 'name'>" +
+               "<input type= 'submit' value='Greet me!'>" +
+               "</form>" +
+               "</body>" +
+               "</html>";
+
     }
 }
