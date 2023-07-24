6495ccc9b6d0ed042468f3abe90a4b181452355f
jenbilyeu80 <125934039+jenbilyeu80@users.noreply.github.com>
2023-07-24 12:14:19 -0500

    Array

---
+++ Desktop/launchCodeJava/counting-character-studio/src/main/java/org/example/CharacterCounter.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CharacterCounter {
+    public CharacterCounter(){
+
+}
+ public void countCharacters(String wordToCount){
+        char[] arrayOfChars = wordToCount.toCharArray();
+     HashMap<Character,Integer> myCharacterCount = new HashMap<>();
+     for( int i =0; i< arrayOfChars.length; i++){
+
+         char theCharacter = arrayOfChars[i];
+         if(myCharacterCount.containsKey(theCharacter)==false) {
+             myCharacterCount.put(theCharacter, 1);
+         }else {
+             Integer theValue = myCharacterCount.get(theCharacter);
+             theValue = theValue +1;
+             myCharacterCount.put(theCharacter, theValue);
+         }
+         System.out.println(myCharacterCount);
+     }
+ArrayList<Character> myCharacterKeys= new ArrayList<>(myCharacterCount.keySet());
+     for(int i =0; i < myCharacterKeys.size(); i++){
+      Integer theValue= myCharacterCount.get(myCharacterKeys.get(i));
+         System.out.println(myCharacterKeys.get(i) + ":" + theValue);
+
+     }
+    }
+}
---
+++ Desktop/launchCodeJava/counting-character-studio/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CharacterCounter myCharacterCounter = new CharacterCounter();
+        myCharacterCounter.countCharacters("My Practice String");
+    }
+}
