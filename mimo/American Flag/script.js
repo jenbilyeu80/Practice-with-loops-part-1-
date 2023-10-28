9607d45a47b5655f03dc33c59f85a866b605d64d
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-10-28 17:44:28 -0500

    added a for loop to create a flag

---
+++ mimo/American Flag/script.js
@@ -0,0 +1,17 @@
+let counter1 = 0;
+while(counter1 < 5){
+    console.log("**********---------");
+    counter1++;
+}
+let counter2 = 0;
+while(counter2 < 4){
+    console.log("-------------------");
+    counter2++;
+}
+
+for (let i=0; i<4; i++){
+console.log("*********----------")
+}
+for(let i=0; i<4; i++){
+    console.log("-------------------")
+}
