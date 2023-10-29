4d503de5bbd508b5fb51dafdb6af9d7e877f08e5
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-10-28 22:04:34 -0500

    while loops

---
+++ mimo/loop practice/script.js
@@ -0,0 +1,25 @@
+/* Complete the while loop body such that each iteratioin increases count by 1 and then use console.log() to display the current count.
+Run the code to check your answer.  When finished, your console should list the numbers 1-10.
+1.  First, make the while loop increment count by 1.
+2. Then, make the loop console.log() the current count.*/
+
+
+
+/*let count = 0;
+while (count < 10){
+    count = count + 1;
+    console.log(count); 
+}*/
+
+/*for(let i = 1; i<11;i++){
+    console.log(i);
+}*/
+
+//more practice with while loop
+
+let count = 16;
+while(count > 0){
+    count = count - 1;
+    console.log(count);
+
+}
