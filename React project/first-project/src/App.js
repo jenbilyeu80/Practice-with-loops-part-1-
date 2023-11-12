bc55f48561694f4ff16214537d73e281f43437a1
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-11-12 15:50:07 -0600

    First React

---
+++ React project/first-project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>HELLO</h1>
+       <p>Welcome to your first React Class Component</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
