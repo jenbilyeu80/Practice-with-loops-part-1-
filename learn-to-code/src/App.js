2f846b70ab8b87ba060003a3d75db3963b51e5e5
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-11-12 16:45:23 -0600

    Learn to Code with mimo using react

---
+++ learn-to-code/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h2>Where can I learn how to code?</h2>
+          <p>Looking for a way to learn how to code easily?<br />
+          Have no fear: MIMO is here to help!
+          </p>
+     <a href="https://mimo.org">MIMO Website</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
