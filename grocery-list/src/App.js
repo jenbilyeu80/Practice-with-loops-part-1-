38a4e65e716a0273fc2dbf9f70889e783c026604
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-11-12 16:14:11 -0600

    Added a Grocery List

---
+++ grocery-list/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div> 
+        <h3>My Groceries List:</h3>
+        <ul>
+          <li>Carrots</li>
+          <li>Milk</li>
+          <li>Apples</li>
+        </ul>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
