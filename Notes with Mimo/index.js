f375b6edf32d81e18869a9f7b8745e8898aa2e03
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-11-24 09:56:43 -0600

    Notes

---
+++ Notes with Mimo/index.js
@@ -0,0 +1,11 @@
+class Car{
+    constructor(year, engine){
+        this.year = year;
+        this.engine = engine;
+    }
+    }
+    const myCar=new Car(2021, "V8");
+    const oldTimer= new Car("1980", "diesel");
+console.log(myCar);
+console.log(oldTimer);
+
