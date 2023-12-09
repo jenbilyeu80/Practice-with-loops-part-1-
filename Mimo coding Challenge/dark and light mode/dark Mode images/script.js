9802bc33ed83a1ed4d5321b21ac7934c23393187
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-12-09 14:50:26 -0600

    toggle Dark Mode

---
+++ Mimo coding Challenge/dark and light mode/dark Mode images/script.js
@@ -0,0 +1,5 @@
+function toggleDarkMode(){
+    const el=
+    document.querySelector("body");
+    el.classList.toggle("darkMode");
+}
---
+++ Mimo coding Challenge/dark and light mode/script.js
@@ -0,0 +1,9 @@
+function lightMode(){
+    const el=document.querySelector("link");
+    el.href="styleLight.css";
+}
+
+function darkMode(){
+    const el= document.querySelector("link");
+    el.href="styleDark.css";
+}
