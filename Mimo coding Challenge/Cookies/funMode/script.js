1cff9aa3b1ec9e1f4427f4ba8024afa5099dcab4
Jennifer Kelly <jenbilyeu@yahoo.com>
2023-12-09 09:32:21 -0600

    Added a FunMode button

---
+++ Mimo coding Challenge/Cookies/funMode/script.js
@@ -0,0 +1,10 @@
+function funMode(){
+    const body=
+    document.querySelector("body");
+    body.style.backgroundColor="violet";
+    body.style.color="darkViolet";
+    const h1=document.querySelector("h1");
+    h1.style.backgroundColor="aqua";
+    const hours= document.querySelector("#hours");
+    hours.style.backgroundColor="paleGreen";
+}
