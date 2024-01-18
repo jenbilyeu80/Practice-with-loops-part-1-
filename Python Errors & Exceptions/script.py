7c907c1214af2896d6e4289a5d1b89dc20f4c3a0
Jennifer Kelly <jenbilyeu@yahoo.com>
2024-01-17 22:59:09 -0600

    Errors

---
+++ Python Errors & Exceptions/script.py
@@ -0,0 +1,24 @@
+# Python Mimo Errors and Exceptions 
+
+
+# Sammy is new to Python and is trying to create a name variable with "Sammy" as its value.
+# When Sammy tries to run the code, a NameError comes up.  Fix the code to successfully save the value in the variable.
+
+name = "Sammy"
+
+# A driving center requested you to code a program to check the age of the students.  The program is meant to print 
+# "Underaged!"  If age is less than 18.  However, the code was poorly written and there is a SyntaxError and IdentationError.  
+# Fix these errors so that the program can rum properly!
+
+age=16
+if age <18:
+    print("Underaged!")
+
+# 3
+# Jamie's trying to create a calculator but is facing a TypeError in the addition operation.  
+# Fix the code so that the console prints 4 after the calculation.
+    
+    first_number = 3
+    second_number = 1
+
+    print(first_number + second_number) 
