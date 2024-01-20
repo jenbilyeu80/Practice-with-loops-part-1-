07edfc26a3cb3bead3cbd7ee814023e41b9a5613
Jennifer Kelly <jenbilyeu@yahoo.com>
2024-01-19 21:16:51 -0600

    DOM challenge 4

---
+++ DOM & Events/DOM Challenge /index.js
@@ -0,0 +1,1 @@
+document.body.firstElementChild.textContent="Employee Profile";
---
+++ DOM & Events/script.js
@@ -0,0 +1,18 @@
+const div = document.getElementById("parent");
+const button = document.getElementById("child");
+//  div.removeChild(button);
+
+
+const list = 
+document.getElementById ("stars");
+const sun = document.getElementById("second");
+list.removeChild(sun);
+
+
+const body = document.getElementById("main");
+const shopName = document.getElementById("shop")
+
+const bod = document.getElementById("parent");
+const child =document.getElementById("motto") 
+
+// const div = document.getElementById("section");
