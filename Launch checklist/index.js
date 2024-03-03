f243fa106dcb5891635c004f05634dfca51a1796
Jennifer Kelly <jenbilyeu@yahoo.com>
2024-03-03 12:03:11 -0600

    Launch Checklist

---
+++ Launch checklist/index.js
@@ -0,0 +1,72 @@
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount= 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 7484231;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let fuelTempCelsius = -225;
+let fuelLevel= 100;
+let weatherStatus = "clear";
+
+console.log("------------------------------------------");
+console.log( ">" + " " + "LC04" + " " + "-" + " " +  "LAUNCH CHECKLIST" );
+console.log("------------------------------------------");
+console.log("Date:"+ " " + date);
+console.log("Time:"+ " " + time);
+console.log("                                          ");
+console.log("                                          ");
+console.log("------------------------------------------");
+console.log(">"+ " " + "ASTRONAUT" + " " + "INFO")
+console.log("------------------------------------------");
+console.log("*" + " " + "count:" + " " + 7 );
+console.log("*" + " " + "status:" + " " + "ready" );
+console.log("                                          ");
+console.log("                                          ");
+console.log("------------------------------------------");
+console.log( ">" + " " + "FUEL DATA" );
+console.log("------------------------------------------");
+console.log("*" + " " + "Fuel" + " " + "temp"+ " " + "celsius:"+ " " +fuelTempCelsius + " " +"C");
+console.log("*" + " " + "Fuel" + " " + "level:"+ " " +fuelLevel + "%");
+console.log("                                          ");
+console.log("                                          ");
+console.log("------------------------------------------");
+console.log( ">" + " " + "MASS DATA" );
+console.log("------------------------------------------");
+console.log("*" + " " + "Crew mass:" + " " + 564.9 + " " + "kg" );
+console.log("*" + " " + "Fuel mass:" + " " + 7600000 + " " + "kg" );
+console.log("*" + " " + "Shuttle mass:" + " " + 74842.31 + " " + "kg" );
+console.log("*" + " " + "Total mass:" + " " + 835407.21 + " " + "kg" );
+console.log("                                          ");
+console.log("                                          ");
+console.log("------------------------------------------");
+console.log( ">" + " " + "FLIGHT PLAN" );
+console.log("------------------------------------------");
+console.log("*" + " " + "weather:" + " " + "clear" );
+console.log("                                          ");
+console.log("                                          ");
+console.log("------------------------------------------");
+console.log( ">" + " " + "OVERALL STATUS" );
+console.log("------------------------------------------");
+console.log("*" + " " + "Clear for takeoff:" + " " + "YES" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
